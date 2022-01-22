38fb95c0c0039c38864bc792d09a2a8f56d3c66e
matt <ames1mw@gmail.com>
2022-01-22 11:14:20 -0500

    commit rapid react

---
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -0,0 +1,22 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DrivetrainSubsystem;
+
+public class DriveCommand extends CommandBase {
+    private final XboxController controller;
+    private final DrivetrainSubsystem drivetrain;
+
+    public DriveCommand(DrivetrainSubsystem drivetrain, XboxController controller){
+        this.drivetrain = drivetrain;
+        this.controller = controller;
+    }
+
+    @Override
+    public void execute(){
+        var percentLeft = controller.getLeftY();
+        var percentRight = controller.getRightY();
+        this.drivetrain.setSpeed(percentLeft,percentRight);
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -0,0 +1,43 @@
+// ROBOTBUILDER TYPE: Subsystem.
+
+package frc.robot.subsystems;
+
+import frc.Hardware.QTalonFX;
+import frc.Hardware.MotorConfigUtils;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DrivetrainSubsystem extends SubsystemBase {
+    private QTalonFX driveMotorLeftA = new QTalonFX(1, 434.68, MotorConfigUtils.POSITION_SLOT_IDX, MotorConfigUtils.VELOCITY_SLOT_IDX);
+    private QTalonFX driveMotorLeftB = new QTalonFX(2, 434.68, MotorConfigUtils.POSITION_SLOT_IDX, MotorConfigUtils.VELOCITY_SLOT_IDX);
+
+    private QTalonFX driveMotorRightA = new QTalonFX(3, 434.68, MotorConfigUtils.POSITION_SLOT_IDX, MotorConfigUtils.VELOCITY_SLOT_IDX);
+    private QTalonFX driveMotorRightB = new QTalonFX(4, 434.68, MotorConfigUtils.POSITION_SLOT_IDX, MotorConfigUtils.VELOCITY_SLOT_IDX);
+
+    private QTalonFX[] driveMotorLefts = new QTalonFX[]{driveMotorLeftA,driveMotorLeftB};
+    private QTalonFX[] driveMotorRights = new QTalonFX[]{driveMotorRightA,driveMotorRightB};
+
+    private final double deadzone = 0.2;
+    //for when we create our own falcon class
+
+    public void setSpeed(double rightSpeed, double leftSpeed){
+        for (var motor : driveMotorLefts){
+            if (Math.abs(leftSpeed) > deadzone){
+                motor.setPercent(rightSpeed);
+            }            
+            if (Math.abs(leftSpeed) < deadzone && Math.abs(rightSpeed) > deadzone){
+                motor.setPercent(-rightSpeed);
+            }
+        }
+        
+        for (var motor : driveMotorRights){
+            if (Math.abs(rightSpeed) > deadzone){
+                motor.setPercent(rightSpeed);
+            }
+            if (Math.abs(rightSpeed) < deadzone && Math.abs(leftSpeed) > deadzone){
+                motor.setPercent(-leftSpeed);
+            }
+        }
+    }
+
+}
