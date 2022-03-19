0b34025797b2029503177a8bee7b66372a93b5bd
Matty Ames <ames1mw@gmail.com>
2022-03-19 14:18:22 -0400

    Trying a new approach

--- src/main/java/frc/robot/commands/SkyHookCommand.java
+++ src/main/java/frc/robot/commands/SkyHookCommand.java
@@ -1,17 +1,15 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.SkyHookSubsystem;
-import edu.wpi.first.wpilibj.Joystick; 
-
+import edu.wpi.first.wpilibj.Joystick;
 
 public class SkyHookCommand extends CommandBase {
     private final Joystick leftJoystick;
     private final Joystick rightJoystick;
     private final SkyHookSubsystem climber;
 
-    public SkyHookCommand(SkyHookSubsystem climber, Joystick leftJoystick, Joystick rightJoystick){
+    public SkyHookCommand(SkyHookSubsystem climber, Joystick leftJoystick, Joystick rightJoystick) {
         this.climber = climber;
         this.rightJoystick = rightJoystick;
         this.leftJoystick = leftJoystick;
@@ -19,9 +17,19 @@ public class SkyHookCommand extends CommandBase {
     }
 
     @Override
-    public void execute(){
-        var climbSpeedUp = rightJoystick.getRawButton(2);
-        var climbSpeedDown = rightJoystick.getRawButton(3);
-        this.climber.setspeed(climbSpeedUp,climbSpeedDown);
+    public void execute() {
+        this.climber.setspeed(getSpeed());
+    }
+
+    private double getSpeed() {
+        if (rightJoystick.getRawButton(2)) {
+            return 1.0d;
+        }
+
+        if (rightJoystick.getRawButton(3)) {
+            return -1.0d;
+        }
+
+        return 0d;
     }
 }
--- src/main/java/frc/robot/subsystems/SkyHookSubsystem.java
+++ src/main/java/frc/robot/subsystems/SkyHookSubsystem.java
@@ -1,27 +1,15 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
-public class SkyHookSubsystem extends SubsystemBase{
+public class SkyHookSubsystem extends SubsystemBase {
     private TalonFX climbmotorL = new TalonFX(30);
     private TalonFX climbmotorR = new TalonFX(40);
 
-    public void setspeed(boolean climbSpeedUp, boolean climbSpeedDown){
-        if (climbSpeedUp){
-            climbmotorR.set(ControlMode.PercentOutput, 1);
-            climbmotorL.set(ControlMode.PercentOutput, 1);
-        }
-        else if (climbSpeedDown){
-            climbmotorR.set(ControlMode.PercentOutput, -1);
-            climbmotorL.set(ControlMode.PercentOutput, -1);
-
-        }
-        else {
-           climbmotorR.set(ControlMode.PercentOutput, 0);
-           climbmotorL.set(ControlMode.PercentOutput, 0);
-        }
+    public void setspeed(double speed) {
+        climbmotorR.set(ControlMode.PercentOutput, speed);
+        climbmotorL.set(ControlMode.PercentOutput, speed);
     }
 }
